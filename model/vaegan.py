69511eb4065a617760ee687e8f2d030d96c0c0a6
JeremyCCHsu <jeremycchsu@gmail.com>
2016-11-29 17:59:52 +0800

    Version 0.3 VAE-GAN successfully implemented and tested. *The `interpolate` API seems to work just fine.  I'll make it to interpolate an areas surrounded by 4 inputs. *Currently, it's very slow (.5 sec per 64 samples)

--- model/vaegan.py
+++ model/vaegan.py
@@ -1,11 +1,45 @@
+import pdb
 from tensorflow.contrib import slim
 import tensorflow as tf
+import numpy as np
 
 # [TODO] I think the upsampling used too much convs (it's OK but I probably have to try less first)
 
 L2 = 1e-6
 # L2 = 0.001
 STDDEV = 0.02
+EPSILON = 1e-10
+
+
+def GaussianLogDensity(x, mu, log_var, name='GaussianLogDensity'):
+    c = np.log(2 * np.pi)
+    var = tf.exp(log_var)
+    x_mu2 = tf.square(tf.sub(x, mu))   # [Issue] not sure the dim works or not?
+    x_mu2_over_var = tf.div(x_mu2, var + EPSILON)
+    log_prob = -0.5 * (c + log_var + x_mu2_over_var)
+    log_prob = tf.reduce_sum(log_prob, -1, name=name)   # keep_dims=True,
+    return log_prob
+
+
+def kld_of_gaussian(mu1, log_var1, mu2, log_var2):
+    ''' Kullback-Leibler divergence of two Gaussians
+        *Assuming that each dimension is independent
+        mu: mean
+        log_var: log variance
+        Equation: http://stats.stackexchange.com/questions/7440/kl-divergence-between-two-univariate-gaussians
+    '''
+    var = tf.exp(log_var1)
+    var2 = tf.exp(log_var2)
+    mu_diff_sq = tf.square(tf.sub(mu1, mu2))
+    single_variable_kld = 0.5 * (log_var2 - log_var1) \
+        + 0.5 * tf.div(var, var2) * (tf.add(1.0, mu_diff_sq)) - 0.5
+    return tf.reduce_sum(single_variable_kld, -1)
+
+def SampleLayer(z_mu, z_lv, name='SampleLayer'):
+    with tf.name_scope(name):
+        eps = tf.random_normal(tf.shape(z_mu))
+        std = tf.sqrt(tf.exp(z_lv))
+        return tf.add(z_mu, tf.mul(eps, std))
 
 def lrelu(x, leak=0.2, name="lrelu"):
   return tf.maximum(x, leak*x)
@@ -16,14 +50,45 @@ class DCGAN(object):
         self.is_training = is_training
 
         # then I have to declare x here, then the shape is determined (undesirable!)
-        self.generate = tf.make_template(
+        self._generate = tf.make_template(
             'Generator',
-            self.generator)
-        self.discriminate = tf.make_template(
+            self._generator)
+        self._discriminate = tf.make_template(
             'Discriminator',
-            self.discriminator)
+            self._discriminator)
+        self._encode = tf.make_template(
+            'Encoder',
+            self.encoder)
 
-    def generator(self, z, is_training):
+
+    def encoder(self, x, is_training):
+        with slim.arg_scope(
+                [slim.batch_norm],
+                scale=True,
+                updates_collections=None,
+                is_training=is_training,
+                reuse=None):
+            with slim.arg_scope(
+                    [slim.conv2d],
+                    kernel_size=[5, 5],
+                    stride=[2, 2],
+                    normalizer_fn=slim.batch_norm,
+                    activation_fn=lrelu):
+                for i in range(4):
+                    x = slim.conv2d(x, self.arch['ch_D'] * (2 ** i))
+                x = slim.flatten(x)
+                # print(x, x.get_shape())
+                z_mu = slim.fully_connected(x, self.arch['z_dim'],
+                    normalizer_fn=None,
+                    activation_fn=None)
+                z_lv = slim.fully_connected(x, self.arch['z_dim'],
+                    normalizer_fn=None,
+                    activation_fn=None)
+        return z_mu, z_lv
+
+
+
+    def _generator(self, z, is_training):
         # Fixed upsampling rate
         w = int(self.arch['img_w'] // 16)
         h = int(self.arch['img_h'] // 16)
@@ -72,7 +137,7 @@ class DCGAN(object):
                     activation_fn=tf.nn.tanh)
         return x
 
-    def discriminator(self, x, is_training):
+    def _discriminator(self, x, is_training):
         ch = self.arch['ch_D']
 
         with slim.arg_scope(
@@ -93,6 +158,7 @@ class DCGAN(object):
                 # Radford: not applying batchnorm to the discriminator input layer
                 x = slim.conv2d(x, ch)
                 # =========== [TEST] =============
+                # J: This seemed to be harmless
                 x = slim.batch_norm(x, scope='Bn-1')
                 # ================================
                 x = lrelu(x)
@@ -103,23 +169,34 @@ class DCGAN(object):
         
         # Don't apply BN for the last layer
         x = slim.flatten(x)
+        h = x
         x = slim.fully_connected(x, 1,
             # weights_regularizer=slim.l2_regularizer(L2)
             activation_fn=None)
-        return x  # no explicit `sigmoid`
+        return x, h  # no explicit `sigmoid`
 
     def loss(self, x):
         batch_size = x.get_shape().as_list()[0]
         # [TODO] Maybe I should make sampling stratified
-        z = tf.random_uniform(
-            shape=[batch_size, self.arch['z_dim']],
-            minval=-1.,
-            maxval=1.,
-            name='z')
-        xh = self.generate(z, is_training=self.is_training)
+        # z = tf.random_uniform(
+        #     shape=[batch_size, self.arch['z_dim']],
+        #     minval=-1.,
+        #     maxval=1.,
+        #     name='z')
+        # z_mu = z
+        # z_lv = z
+
+        z_mu, z_lv = self._encode(x, is_training=self.is_training)
+        z = SampleLayer(z_mu, z_lv)
+        # print(z_mu.get_shape(), z_lv.get_shape())
+
+        xh = self._generate(z, is_training=self.is_training)
         self.xh = xh
-        logit_fake = self.discriminate(xh, is_training=self.is_training)
-        logit_true = self.discriminate(x, is_training=self.is_training)
+
+        # pdb.set_trace()
+
+        logit_fake, xh_through_D = self._discriminate(xh, is_training=self.is_training)
+        logit_true, x_through_D = self._discriminate(x, is_training=self.is_training)
 
         with tf.name_scope('loss'):
             loss = dict()
@@ -138,6 +215,17 @@ class DCGAN(object):
                     logit_fake,
                     tf.ones_like(logit_fake)))
 
+            loss['KL(z)'] = tf.reduce_mean(
+                kld_of_gaussian(
+                    z_mu, z_lv,
+                    tf.zeros_like(z_mu), tf.zeros_like(z_lv)))
+
+            loss['Dis'] = - tf.reduce_mean(
+                GaussianLogDensity(
+                    x_through_D,
+                    xh_through_D,
+                    tf.zeros_like(xh_through_D)))
+
         return loss
 
     def sample(self, z=128):
@@ -154,3 +242,30 @@ class DCGAN(object):
         return self.xh  # [BUG] called before assigned
         # return xh
 
+
+    def encode(self, x):
+        z_mu, z_lv = self._encode(x, is_training=False)
+        return dict(mu=z_mu, log_var=z_lv)
+
+    def decode(self, z, y=None):
+        return self._generate(z, is_training=False)
+        
+    def interpolate(self, x1, x2, n):
+        ''' Interpolation from the latent space '''
+        # z's should be 1x100
+        z1, _ = self._encode(
+            tf.expand_dims(x1, 0),
+            is_training=False)
+        z2, _ = self._encode(
+            tf.expand_dims(x2, 0),
+            is_training=False)
+        a = tf.reshape(tf.linspace(0., 1., n), [n, 1])
+
+        z1 = tf.matmul(1. - a, z1)
+        z2 = tf.matmul(a, z2)
+        z = tf.add(z1, z2)
+        # z = tf.add(z1, tf.matmul(a, z2))
+        xh = self._generate(z, is_training=False)
+        # xh = tf.transpose(xh, [1, 2, 3, 0])
+        # xh = tf.reshape()
+        return xh
--- train.py
+++ train.py
@@ -4,6 +4,8 @@ import time
 import json
 import tensorflow as tf
 
+from datetime import datetime
+
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
@@ -17,13 +19,31 @@ lr = 2e-4
 beta1 = 0.5
 datadir = '/home/jrm/proj/Hanzi/TWKai98_32x32'
 n_epoch = 100
-n_step = 16300
+# n_step = 16300
 batch_size = 64
 
+STARTED_DATESTRING = datetime.now().strftime('%0m%0d-%0H%0M-%0S-%Y')
+N_INTERP = 10
+LOGDIR_ROOT = './logdir'
 
+args = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string(
+    'datadir', '/home/jrm/proj/Hanzi/TWKai_98_32x32', 'dir to dataset')
+tf.app.flags.DEFINE_string(
+    'logdir_root', None, 'root of log dir')
+tf.app.flags.DEFINE_string(
+    'logdir', None, 'log dir')
+tf.app.flags.DEFINE_string(
+    'restore_from', None, 'restore from dir (not from *.ckpt)')
+tf.app.flags.DEFINE_integer('n_epoch', 100, 'num of epoch')
+tf.app.flags.DEFINE_integer('batch_size', 64, 'batch size')
+tf.app.flags.DEFINE_float('lr', 2e-4, 'learning rate')
+tf.app.flags.DEFINE_float('beta1', 0.5, 'beta1 in AdamOptimizer')
+tf.app.flags.DEFINE_boolean('train', False, 'is training or not')
 
-
+# [TODO] should I put this in a util dir?
 def save(saver, sess, logdir, step):
+    ''' Save a model to logdir/model.ckpt-[step] '''
     model_name = 'model.ckpt'
     checkpoint_path = os.path.join(logdir, model_name)
     print('Storing checkpoint to {} ...'.format(logdir), end="")
@@ -36,101 +56,198 @@ def save(saver, sess, logdir, step):
     print(' Done.')
 
 
-# x = tf.placeholder(tf.float32, shape=[128, 32, 32, 1])
-
-coord = tf.train.Coordinator()
-imgs, info = img_reader(
-	datadir=datadir,
-	img_dims=(32, 32, 1),
-	batch_size=batch_size,
-	rtype='tanh')
-	# num_examples_per_epoch=20941)
-
-step_per_ep = info['n_files'] // batch_size
-# g = sess.run(imgs)
-
-arch = json.load(open('architecture.json'))
-
-machine = DCGAN(arch, is_training=True)
-
-loss = machine.loss(imgs)
-xh = machine.sample(batch_size)
-
-trainables = tf.trainable_variables()
-g_vars = [v for v in trainables if 'Generator' in v.name]
-d_vars = [v for v in trainables if 'Discriminator' in v.name]
-
-# # print(g_vars)
-# print('\nGenerator')
-# for v in g_vars:
-# 	print(v.name)
-# print('\n\n\nDiscriminator')
-# # print(d_vars)
-# for v in d_vars:
-# 	print(v.name)
-
-# optimizer = tf.train.AdamOptimizer(lr)
-obj_D = loss['D_fake'] + loss['D_real']
-
-optimizer = tf.train.AdamOptimizer(lr, beta1)
-# optimizer = tf.train.RMSPropOptimizer(lr)
-
-opt_d = optimizer.minimize(
-	obj_D,
-	var_list=d_vars)
-opt_g = optimizer.minimize(
-	loss['G_fake'],
-	var_list=g_vars)
+def load(saver, sess, logdir):
+    '''
+    Try to load model form a dir (search for the newest checkpoint)
+    '''
+    print('Trying to restore checkpoints from {} ...'.format(logdir),
+        end="")
+    ckpt = tf.train.get_checkpoint_state(logdir)
+    if ckpt:
+        print('  Checkpoint found: {}'.format(ckpt.model_checkpoint_path))
+        global_step = int(
+            ckpt.model_checkpoint_path
+            .split('/')[-1]
+            .split('-')[-1])
+        print('  Global step: {}'.format(global_step))
+        print('  Restoring...', end="")
+        saver.restore(sess, ckpt.model_checkpoint_path)
+        return global_step
+    else:
+        print('No checkpoint found')
+        return None
 
 
-writer = tf.train.SummaryWriter(logdir)
-writer.add_graph(tf.get_default_graph())
+def visualize_random_samples(sess, imgs, xh, filename=None):
+    img_real = sess.run(imgs)
+    img_fake = sess.run(xh)
+    
+    plt.figure()
+    for i in range(15):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(img_fake[i, :, :, 0], interpolation='none', cmap='gray')
+        plt.colorbar()
+    plt.subplot(4, 4, 16)
+    plt.imshow(img_real[0, :, :, 0], interpolation='none', cmap='gray')
+    plt.colorbar()
 
-sess = tf.Session()
-init = tf.initialize_all_variables()
-sess.run(init)
-
-saver = tf.train.Saver()
-
-threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-
-
-# for epoch in range(n_epoch):
-try:
-
-	n_iter = info['n_files'] // batch_size
-	time_i = time.time()
-	for ep in range(n_epoch):
-		for it in range(n_iter):
-
-			_, l_df, l_dr = sess.run([opt_d, loss['D_fake'], loss['D_real']])
-			_, l_g = sess.run([opt_g, loss['G_fake']])
-			t = time.time() - time_i
-			print('Epoch [{:3d}/{:3d}] [{:4d}/{:4d}] d_loss={:6.3f}+{:6.3f}, g_loss={:5.2f}, T={:.2f}'
-				.format(ep, n_epoch, it, n_iter, l_df, l_dr, l_g, t))	
-
-			if it % (n_iter // 2) == 0:	
-				creation = sess.run(xh)
-				real = sess.run(imgs)
-	
-				# [TODO] functionize
-				plt.figure()
-				for i in range(15):
-					plt.subplot(4, 4, i + 1)
-					plt.imshow(creation[i, :, :, 0], interpolation='none', cmap='gray')
-					plt.colorbar()
-				plt.subplot(4, 4, 16)
-				plt.imshow(real[0, :, :, 0], interpolation='none', cmap='gray')
-				plt.colorbar()
-				plt.savefig('test-Ep{:03d}-It{:04d}.png'.format(ep, it))
-				plt.close()
+    if filename:
+        plt.savefig(filename)
+        plt.close()
 
 
-except KeyboardInterrupt:
-	print()
+def visualize_interpolation(sess, x_interp, filename=None):
+    x_ = sess.run(x_interp)
+    plt.figure(figsize=(x_.shape[0], 1))
+    for i in range(x_.shape[0]):
+        plt.subplot(1, x_.shape[0], i + 1)
+        plt.imshow(x_[i, :, :, 0], interpolation='none', cmap='gray')
+        plt.axis('off')
+        # plt.colorbar()
+    
+    if filename:
+        plt.savefig(filename)
+        plt.close()
 
-finally:
-	save(saver, sess, logdir, ep*n_iter + it)
-	coord.request_stop()
-	coord.join(threads)
+
+def get_optimization_ops(loss):
+    trainables = tf.trainable_variables()
+    g_vars = [v for v in trainables if 'Generator' in v.name]
+    d_vars = [v for v in trainables if 'Discriminator' in v.name]
+    e_vars = [v for v in trainables if 'Encoder' in v.name]
+
+    optimizer = tf.train.AdamOptimizer(args.lr, args.beta1)
+
+    # # Vanila GAN
+    # optimizer = tf.train.AdamOptimizer(args.lr)
+    # obj_D = loss['D_fake'] + loss['D_real']
+    # obj_G = loss['G_fake']
+    # optimizer = tf.train.RMSPropOptimizer(args.lr)
+    # opt_d = optimizer.minimize(
+    #   obj_D,
+    #   var_list=d_vars)
+    # opt_g = optimizer.minimize(
+    #   loss['G_fake'],
+    #   var_list=g_vars)
+
+    # VAE-GAN
+    obj_D = loss['D_fake'] + loss['D_real']
+    obj_G = loss['Dis'] + loss['G_fake']
+    obj_E = loss['KL(z)'] + loss['Dis']
+
+    opt_d = optimizer.minimize(obj_D, var_list=d_vars)
+    opt_g = optimizer.minimize(obj_G, var_list=g_vars)
+    opt_e = optimizer.minimize(obj_E, var_list=e_vars)
+    return opt_d, opt_g, opt_e
+
+
+def get_default_logdir(logdir_root):
+    return  os.path.join(logdir_root, 'train', STARTED_DATESTRING)
+
+def validate_log_dirs(args):
+    if args.logdir and args.restore_from:
+        raise ValueError(
+            'You can only specify one of the following: ' +
+            '--logdir and --restoreform')
+
+    if args.logdir and args.log_root:
+        raise ValueError('You can only specify either --logdir' +
+            'or --logdir_root')
+
+    if args.logdir_root is None:
+        logdir_root = LOGDIR_ROOT
+
+    if args.logdir is None:
+        logdir = get_default_logdir(logdir_root)
+        print('Using default logdir: {:s}'.format(logdir))
+
+    # Note: `logdir` and `restore_from` are exclusive
+    if args.restore_from is None:
+        restore_from = logdir
+
+    return dict(logdir=logdir,
+        logdir_root=logdir_root,
+        restore_from=restore_from)
+
+# [TODO] load model for more work
+
+def main():
+    '''
+    Note:
+      1. The input is rescaled to [-1, 1] (img_reader: rtype)
+    '''
+    dirs = validate_log_dirs(args)
+
+    coord = tf.train.Coordinator()
+    arch = json.load(open('architecture.json'))
+    imgs, info = img_reader(
+        datadir=datadir,
+        img_dims=(arch['img_h'], arch['img_w'], arch['img_c']),
+        batch_size=batch_size,
+        rtype='tanh')
+    
+    machine = DCGAN(arch, is_training=True)
+
+    loss = machine.loss(imgs)
+    xh = machine.sample(batch_size)
+    
+    x_interp = machine.interpolate(imgs[0], imgs[1], N_INTERP)
+
+    opt_d, opt_g, opt_e = get_optimization_ops(loss)
+
+    writer = tf.train.SummaryWriter(dirs['logdir'])
+    writer.add_graph(tf.get_default_graph())
+
+    sess = tf.Session()
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+    saver = tf.train.Saver()
+    try:
+        saved_global_step = load(saver, sess, dirs['restore_from'])
+        if saved_global_step is None:
+            saved_global_step = -1
+    except:
+        print("Something's wrong while restoing checkpoints!")
+        raise
+
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    # ========== Actual training loop ==========
+    try:
+        n_iter_per_epoch = info['n_files'] // batch_size
+        time_i = time.time()
+        for ep in range(n_epoch):
+            for it in range(n_iter_per_epoch):
+                _, l_df, l_dr = sess.run([opt_d, loss['D_fake'], loss['D_real']])
+                _, l_g = sess.run([opt_g, loss['G_fake']])
+                _, l_e, l_dis = sess.run([opt_e, loss['KL(z)'], loss['Dis']])
+
+                t = time.time() - time_i
+
+                print(
+                    'Epoch [{:3d}/{:3d}] '.format(ep, n_epoch) +
+                    '[{:4d}/{:4d}] '.format(it, n_iter_per_epoch) +
+                    'd_loss={:6.3f}+{:6.3f}, '.format(l_df, l_dr) +
+                    'g_loss={:5.2f}, T={:.2f}'.format(l_g, t)
+                    )
+
+                if it % (n_iter_per_epoch // 2) == 0:   
+                    # visualize_random_samples(sess, imgs, xh,
+                    #   filename='test-Ep{:03d}-It{:04d}.png'.format(ep, it))
+                    visualize_interpolation(sess, x_interp,
+                        filename=os.path.join(
+                            dirs['logdir'],
+                            'test-Ep{:03d}-It{:04d}.png'.format(ep, it)))
+
+    except KeyboardInterrupt:
+        print()
+
+    finally:
+        save(saver, sess, dirs['logdir'], ep * n_iter_per_epoch + it)
+        coord.request_stop()
+        coord.join(threads)
+
+if __name__ == '__main__':
+    main()
 
